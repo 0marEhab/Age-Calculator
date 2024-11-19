41b81667e2f85fdd6200e8bf4b60a0745fdf8c99
Omar Ehab <mooroehab18@gmail.com>
2024-11-19 21:45:06 +0200

    first commit

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -8,7 +8,7 @@ export default function Header({ content, toggleSidebar }) {
         <div className="w-[1220px] flex justify-between items-start font-os text-lg">
           <div className="flex gap-8 justify-center items-center">
             <button onClick={toggleSidebar}>
-              <GiHamburgerMenu />
+              {<GiHamburgerMenu /> || sidebar}
             </button>
             <Link to={"/en"}>AR</Link>
           </div>
--- src/components/SocialMedia.jsx
+++ src/components/SocialMedia.jsx
@@ -3,23 +3,24 @@ import { RiWhatsappFill } from "react-icons/ri";
 import { FaLinkedinIn } from "react-icons/fa";
 import { FaTwitter } from "react-icons/fa";
 import { FaFacebookF } from "react-icons/fa";
+import { Link } from "react-router-dom";
 export default function SocialMedia() {
   return (
     <div className="flex font-os mt-10 text-[#212529] gap-5 flex-col justify-center  items-center bg-white p-6">
       <p className=" ">شارك الموقع على شبكات التواصل الاجتماعي</p>
       <div className="flex gap-5  justify-center">
-        <a href="#" className=" px-3 py-3  rounded-md bg-[#F0F2F5] ">
+        <Link className=" px-3 py-3  rounded-md bg-[#F0F2F5] ">
           <RiWhatsappFill size={30} className="text-green-500" />
-        </a>
-        <a href="#" className=" px-3 py-3  rounded-md bg-[#F0F2F5] ">
+        </Link>
+        <Link className=" px-3 py-3  rounded-md bg-[#F0F2F5] ">
           <FaLinkedinIn size={30} className=" text-blue-600" />
-        </a>
-        <a href="#" className=" px-3 py-3  rounded-md bg-[#F0F2F5] ">
+        </Link>
+        <Link className=" px-3 py-3  rounded-md bg-[#F0F2F5] ">
           <FaTwitter size={30} className=" text-blue-400" />
-        </a>
-        <a href="#" className=" px-3 py-3  rounded-md bg-[#F0F2F5] ">
+        </Link>
+        <Link className=" px-3 py-3  rounded-md bg-[#F0F2F5] ">
           <FaFacebookF size={30} className=" text-blue-700" />
-        </a>
+        </Link>
       </div>
     </div>
   );
